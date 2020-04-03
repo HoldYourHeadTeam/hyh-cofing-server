92fc3fe56722894e77f6ffc7295726f285e69a29
Aleksandr.Kulakov <oleksandr.kulakov@nure.ua>
2020-04-03 20:55:48 +0300

    Config commit

--- src/main/java/com/hyh/HyhConfigServerApplication.java
+++ src/main/java/com/hyh/HyhConfigServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class HyhApplication {
+public class HyhConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HyhApplication.class, args);
+		SpringApplication.run(HyhConfigServerApplication.class, args);
 	}
 
 }
